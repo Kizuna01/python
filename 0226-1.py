491eb4b79940e36282830653bedffbdbcffdccbe
Kizuna01 <79702494+Kizuna01@users.noreply.github.com>
2021-02-26 21:49:26 +0800

    Add files via upload

---
+++ 0226-1.py
@@ -0,0 +1,4 @@
+for i in range(10,0,-1):
+    for v in range (1,i):
+      print(v,end='')
+    print()
---
+++ 0226-2.py
@@ -0,0 +1,6 @@
+#1+...+??
+a=int(input('輸入數字'))
+total=0
+for i in range (1,a+1):
+    total+=i
+print(total)
