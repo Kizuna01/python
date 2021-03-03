79deab44c0a027699dff93a47f84717b6b8d91e3
Kizuna01 <79702494+Kizuna01@users.noreply.github.com>
2021-03-03 19:06:08 +0800

    Add files via upload

---
+++ 0226-3.py
@@ -0,0 +1,14 @@
+for i in range(1,8,2):
+    for v in range(i,7,2):
+        print(' ',end='')
+    for s in range(i):
+        print('*',end='')
+    print()
+for i in range(5,-1,-2):
+    for v in range(i,6,2):
+        print(' ',end='')
+    for s in range(i):
+        print('*',end='')
+    
+    print()
+    
